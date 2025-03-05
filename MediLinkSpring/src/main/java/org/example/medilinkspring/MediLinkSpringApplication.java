29b9a21eff1c6efd281e2c68f8f308481134622e
ari <kjk9858@gmail.com>
2025-03-06 01:00:48 +0900

    프로젝트 준비

---
+++ MediLinkSpring/src/main/java/org/example/medilinkspring/MediLinkSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.medilinkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediLinkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediLinkSpringApplication.class, args);
+	}
+
+}
---
+++ MediLinkSpring/src/test/java/org/example/medilinkspring/MediLinkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medilinkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediLinkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

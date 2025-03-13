d9a9ff195a8502ccff625bfab897ab6fa1109ff5
윤경진 <pkyouhg0915@naver.com>
2025-03-13 17:51:31 +0900

    현재 위치로 초기화 실패

--- MediLink-Vue/src/map/pages/map.vue
+++ MediLink-Vue/src/map/pages/map.vue
@@ -9,53 +9,18 @@ import axios from '@/util/http';
 
 const mapRef = ref(null);
 
+// 위치 정보 저장용 ref
+const positionObj = ref({
+  latitude: null,
+  longitude: null,
+});
+
 // 여러 개의 마커 좌표 배열
 const markerData = [
   { lat: 37.5670135, lng: 126.9783740 }, // 서울
   { lat: 37.3595704, lng: 127.105399 },  // 분당
   { lat: 35.1795543, lng: 129.0756416 }, // 부산
 ];
-
-// 네이버 지도 API 로드 함수
-const loadNaverMap = () => {
-  return new Promise((resolve) => {
-    if (window.naver) {
-      resolve(window.naver);
-      return;
-    }
-
-    const script = document.createElement("script");
-    script.src =
-      "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lamhj6mox4";
-    script.async = true;
-    script.defer = true;
-    document.head.appendChild(script);
-
-    script.onload = () => resolve(window.naver);
-  });
-};
-
-onMounted(async () => {
-const naver = await loadNaverMap(); // 네이버 지도 API 로드 완료 후 실행
-
-const map = new naver.maps.Map(mapRef.value, {
-    center: new naver.maps.LatLng(37.5670135, 126.9783740),
-    zoom: 15,     // 초기 줌
-    minZoom: 5,   // 최소 줌
-    scaleControl: false,
-    logoControl: false,
-    mapDataControl: false,
-    zoomControl: true,
-  });
-
-// 마커 객체를 배열로 저장
-const markers = markerData.map((data) => {
-  return new naver.maps.Marker({
-    position: new naver.maps.LatLng(data.lat, data.lng),
-    map: map,
-    title: data.title
-  });
-});
 
 // 마커 보일 영억을 지정
 var HOME_PATH = window.HOME_PATH || '.';
@@ -93,16 +58,35 @@ var MARKER_SPRITE_X_OFFSET = 29,
         "I2": [MARKER_SPRITE_X_OFFSET*8, MARKER_SPRITE_Y_OFFSET*2]
     };
 
-// 지도의 경계를 구함
-var bounds = map.getBounds(),
-southWest = bounds.getSW(),
-northEast = bounds.getNE(),
-lngSpan = northEast.lng() - southWest.lng(),
-latSpan = northEast.lat() - southWest.lat();
+// 네이버 지도 API 로드 함수
+const loadNaverMap = () => {
+  return new Promise((resolve) => {
+    if (window.naver) {
+      resolve(window.naver);
+      return;
+    }
 
-naver.maps.Event.addListener(map, 'idle', function () {
-    updateMarkers(map, markers);
-});
+    const script = document.createElement("script");
+    script.src =
+      "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lamhj6mox4";
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    script.onload = () => resolve(window.naver);
+  });
+};
+
+// 현재 위치 받아오기
+function accessToGeo (position) {
+    positionObj.value.latitude = position.coords.latitude;
+    positionObj.value.longitude = position.coords.longitude;
+    // console.log(positionObj.value); // 여기서 위치 객체를 확인할 수 있습니다.
+}
+
+function askForLocation () {
+    navigator.geolocation.getCurrentPosition(accessToGeo)
+}
 
 function updateMarkers(map, markers) {
     var mapBounds = map.getBounds();
@@ -115,18 +99,58 @@ function updateMarkers(map, markers) {
     }
 }
 
+// 마커를 보이게 함
 function showMarker(map, marker) {
     if (marker.getMap()) return;
     marker.setMap(map);
 }
 
+// 마커를 숨김
 function hideMarker(map, marker) {
     if (!marker.getMap()) return;
     marker.setMap(null);
 }
 
+onMounted(async () => {
+
+await askForLocation();
+console.log(positionObj)
+const naver = await loadNaverMap(); // 네이버 지도 API 로드 완료 후 실행
+
+const map = new naver.maps.Map(mapRef.value, {
+    center: new naver.maps.LatLng(37.5670135, 126.9783740),
+    // center: new naver.maps.LatLng(positionObj.value.latitude, positionObj.value.longitude),
+    zoom: 15,     // 초기 줌
+    minZoom: 7,   // 최소 줌
+    scaleControl: false,
+    logoControl: false,
+    mapDataControl: false,
+    zoomControl: true,
+  });
+
+// 마커 객체를 배열로 저장
+const markers = markerData.map((data) => {
+  return new naver.maps.Marker({
+    position: new naver.maps.LatLng(data.lat, data.lng),
+    map: map,
+    title: data.title
+  });
+});
+
+// 지도의 경계를 구함
+var bounds = map.getBounds(),
+southWest = bounds.getSW(),
+northEast = bounds.getNE(),
+lngSpan = northEast.lng() - southWest.lng(),
+latSpan = northEast.lat() - southWest.lat();
+
+// 마크를 업데이트
+naver.maps.Event.addListener(map, 'idle', function () {
+    updateMarkers(map, markers);
+});
 
 });
+
 </script>
 
 <style scoped>
